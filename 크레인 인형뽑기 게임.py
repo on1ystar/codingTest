e9ed3689b9b5fd0ff911980cf7d842ca10dd89c5
on1ystar <tjdwls0607@naver.com>
2021-01-21 02:42:13 +0900

    크레인 인형뽑기 게임

---
+++ 크레인 인형뽑기 게임.py
@@ -0,0 +1,22 @@
+def solution(board, moves):
+    answer = 0
+    basket = []
+    temp = [0]
+    for move in moves:
+        for i in range(len(board)):
+            if board[i][move-1] != 0:
+                basket.append(board[i][move-1])
+                board[i][move-1] = 0
+                break
+    while True:
+        if len(basket) == 0:
+            break
+        if basket[len(basket)-1] == temp[-1]:
+            basket.pop()
+            temp.pop()
+            answer += 2
+        else:
+            temp.append(basket.pop())
+    return answer
+
+print(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]))
