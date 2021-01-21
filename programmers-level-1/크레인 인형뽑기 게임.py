f1adaa8d53a8d2d0f09f843cfcb15d59d90d11b6
on1ystar <tjdwls0607@naver.com>
2021-01-21 19:54:48 +0900

    add solution-2

--- programmers-level-1/크레인 인형뽑기 게임.py
+++ programmers-level-1/크레인 인형뽑기 게임.py
@@ -1,3 +1,4 @@
+# 방법 1
 def solution(board, moves):
     answer = 0
     basket = []
@@ -18,5 +19,23 @@ def solution(board, moves):
         else:
             temp.append(basket.pop())
     return answer
+
+# 방법 2
+# def solution(board, moves):
+#     answer = 0
+#     basket = [0]
+#     for move in moves:
+#         for i in range(len(board)):
+#             if board[i][move-1] != 0:
+#                 if board[i][move-1] == basket[-1]:
+#                     answer += 2
+#                     board[i][move-1] = 0
+#                     basket.pop()
+#                     break
+#                 else:
+#                     basket.append(board[i][move-1])
+#                     board[i][move-1] = 0
+#                     break
+#     return answer
 
 print(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]))
