405960d6a2e803de8a7b0fb9071690561e1d0321
on1ystar <tjdwls0607@naver.com>
2021-01-30 17:22:43 +0900

    볼링공 고르기

--- greedy/볼링공 고르기.py
+++ greedy/볼링공 고르기.py
@@ -1,10 +1,10 @@
-import collections
-import math
+from collections import Counter
+from math import factorial
 
 n, m  = map(int, input().rstrip().split())
 li = list(map(int, input().rstrip().split()))
-result = math.factorial(n) // (math.factorial(n-2)*2)
-count = collections.Counter(li)
+result = factorial(n) // (factorial(n-2)*2)
+count = Counter(li)
 for i in range(1, m+1):
     n = count[i]
     if n == 1 or n == 0:
@@ -12,6 +12,6 @@ for i in range(1, m+1):
     if n == 2:
         result -= 1
         continue
-    result -= math.factorial(n) // math.factorial(n-2)*2
+    result -= factorial(n) // factorial(n-2)*2
 print(result)
 
