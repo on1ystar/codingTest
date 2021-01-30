1ba1e2c94c4acf3270067e95f84054a7bcb3dcd4
on1ystar <tjdwls0607@naver.com>
2021-01-31 01:03:04 +0900

    무지의 먹방 라이브

--- greedy/무지의 먹방 라이브.py
+++ greedy/무지의 먹방 라이브.py
@@ -1,21 +1,33 @@
+import operator
+from collections import deque
+
+
 def solution(food_times, k):
     answer = 0
-    while answer == 0:
-        n = k // len(food_times)
-        cnt = k % len(food_times)
-        over = 0
-        for i, x in enumerate(food_times):
-            if (x - n) < 0:
-                over += x*(-1)
-                food_times[i] = 0
-            elif (x - n) == 0:
-                food_times[i] = 0
-                cnt -= 1
-            else:
-                food_times[i] -= n
-                cnt -= 1
-            if cnt == 0:
-                answer = i
-                break
-        k = cnt + over
+    food_times_dic = {}
+    for i, v in enumerate(food_times):
+        food_times_dic[i+1] = v
+    sorted_dic_q = deque(sorted(map(list, food_times_dic.items()), key=operator.itemgetter(1)))
+
+    while True:
+        n = k // len(sorted_dic_q)
+        if n >= 1:
+            iter_cnt = min(sorted_dic_q[0][1], n)
+            k -= len(sorted_dic_q) * iter_cnt
+            for food in sorted_dic_q:
+                food[1] -= iter_cnt
+            while True:
+                if food_times_dic == {}:
+                    return -1
+                if sorted_dic_q[0][1] == 0:
+                    i, _ = sorted_dic_q.popleft()
+                    del food_times_dic[i]
+                else:
+                    break
+        else:
+            answer = sorted(food_times_dic.items(), key=operator.itemgetter(0))[k][0]
+            break
     return answer
+
+
+print(solution([3, 1, 2, 1 ,2, 3, 4 ,5, 4, 6 ,7], 11))
