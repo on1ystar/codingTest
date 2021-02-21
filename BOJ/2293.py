1b04e8fe0c3b6d88a4250f2dd15a00ed766e4612
on1ystar <tjdwls0607@naver.com>
2021-02-21 14:39:49 +0900

    BOJ-2293

--- BOJ/2293.py
+++ BOJ/2293.py
@@ -1,18 +1,16 @@
-# from sys import stdin
-#
-# n, k = map(int, input().rstrip().split())
-# coins = []
-# dp = [0] * (k+1)
-# for _ in range(n):
-#     coin = int(stdin.readline().rstrip())
-#     coins.append(coin)
-# for i in range(1, k+1):
-#     temp = []
-#     for coin in coins:
-#         if 0 < (i - coin):
-#             if dp[i - coin] != 0:
-#                 temp.append((i - coin, dp[i - coin]))
-#         elif (i - coin) == 0:
-#             dp[i] += 1
-#     print(temp)
-# print(dp[k])
+from sys import stdin
+
+N, K = map(int, input().rstrip().split())
+coins = []
+dp = [0] * (K+1)
+dp[0] = 1
+for _ in range(N):
+    coins.append(int(stdin.readline().rstrip()))
+coins.sort()
+
+for coin in coins:
+    for i in range(1, K+1):
+        if i//coin == 0:
+            continue
+        dp[i] += dp[i - coin]
+print(dp[K])
