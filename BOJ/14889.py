d1641bbf7e1416f749919ed06f9ccc3fc119f302
on1ystar <tjdwls0607@naver.com>
2021-03-07 03:03:29 +0900

    BOJ-14889

---
+++ BOJ/14889.py
@@ -0,0 +1,22 @@
+# 스타트와 링크
+
+from itertools import combinations
+
+if __name__ == "__main__":
+    N = int(input())
+    S = []
+    for _ in range(N):
+        S.append(list(map(int, input().split())))
+    min_diff = 1000
+    team_start_list = list(combinations(range(N), N//2))
+    for team_start in team_start_list[:len(team_start_list)//2]:
+        team_start_stat, team_link_stat = 0, 0
+        team_link = list(set(range(N)) - set(team_start))
+        for i in team_start:
+            for j in team_start:
+                team_start_stat += S[i][j]
+        for i in team_link:
+            for j in team_link:
+                team_link_stat += S[i][j]
+        min_diff = min(min_diff, abs(team_start_stat - team_link_stat))
+    print(min_diff)
