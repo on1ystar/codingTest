4277ae2ee8d2f2408f9c563e8043e3580eaa586d
on1ystar <tjdwls0607@naver.com>
2021-03-08 21:47:04 +0900

    update BOJ-1697

--- BOJ/1697.py
+++ BOJ/1697.py
@@ -8,19 +8,24 @@ if __name__ == "__main__":
     if N > K:
         print(N-K)
     else:
-        queue = deque()
-        queue.append((N, 0))
+        queue = deque([N])
         visited = [False] * 100001
+        t = 0
         while queue:
-            now_pos, t = queue.popleft()
-            if now_pos == K:
-                print(t)
-                break
-            if visited[now_pos]: continue
-            else: visited[now_pos] = True
-            if 0 < (now_pos * 2) < (2*K - now_pos) and (now_pos * 2) <= 100000:
-                queue.append((now_pos * 2, t+1))
-            if (now_pos + 1) <= K:
-                queue.append((now_pos + 1, t+1))
-            if (now_pos - 1) >= 0: 
-                queue.append((now_pos - 1, t+1))
+            for now_pos in queue.copy():
+                queue.popleft()
+                if now_pos == K:
+                    print(t)
+                    break
+                if visited[now_pos]: continue
+                visited[now_pos] = True
+                if 0 < (now_pos * 2) < (2*K - now_pos) and (now_pos * 2) <= 100000:
+                    queue.append(now_pos * 2)
+                if (now_pos + 1) <= K:
+                    queue.append(now_pos + 1)
+                if (now_pos - 1) >= 0: 
+                    queue.append(now_pos - 1)
+            else:
+                t += 1
+                continue
+            break
